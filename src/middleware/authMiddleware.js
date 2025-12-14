e2057e9e8596a4575ba7b8afbb318a71bbebd7fa
Hassan Jamot <jamothassan675@gmail.com>
2025-12-14 18:49:27 +0100

    JWT auth middleware build

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = authHeader.split("")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+
+    next();
+  } catch (err) {
+    console.error("Auth middleware error:", err);
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
